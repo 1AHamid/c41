21258e0ac6237356f469aad082b4eb534ee01ff6
lumine2412 <78664994+lumine2412@users.noreply.github.com>
2021-08-03 18:49:31 -0400

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,52 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+
+  //to get number of cars from the database and store it in property this.rank
+  getCarsAtEnd(){
+    var carRef = database.ref('carsAtEnd')
+    carRef.on("value",(data)=>{
+      this.rank = data.val();
+    })
+  }
+
+  //to update the number of cars at end in the database
+  static updateCarsAtEnd(rank){
+    //.ref('/') refers to the entire database
+    database.ref('/').update({
+    carsAtEnd: rank
+    })
+  }
+}
